78d20bb202de20d8eee89000ad4504969d798779
Sumit Sehgal <126518503+7654878448@users.noreply.github.com>
2024-07-01 23:51:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+function App()
+{
+  return(
+    <div>
+      <h1 style={{
+        color:'red',
+        backgroundColor:'yellow',
+        font:'80px'
+      }} >jai Shree Ram</h1>
+      <p style={{
+        color:'red',
+        backgroundColor:'orange',
+        font:'98px'
+        
+
+      }}>hello welcome to react application{JSON.stringify(new Date())}</p>
+    </div>
+  )
+}
+export default App

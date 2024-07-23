d3afc05d718c67b567ee3aa5e648fca07d611705
Sumit Sehgal <126518503+7654878448@users.noreply.github.com>
2024-07-23 20:25:38 +0530

    Delete Talk-to-us.jsx

--- Talk-to-us.jsx
+++
@@ -1,9 +0,0 @@
-const Talk=()=>
-    {
-        return(
-            <div>
-                <h1>you can call on this number 765477383</h1>
-            </div>
-        )
-    }
-    export default Talk
